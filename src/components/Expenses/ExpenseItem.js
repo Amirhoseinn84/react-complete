c101f98e38ab7ef73406860faf21e5c0da32661c
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-06-09 10:13:27 +0330

    add new

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,24 +6,24 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
   // function clickhandler() {}
-  const [title, setTtitle] = useState(props.title);
-  console.log('ExpenseItem evaluated by React');
-  // let title = props.title;
+  // const [title, setTtitle] = useState(props.title);
+  // console.log('ExpenseItem evaluated by React');
+  // // let title = props.title;
 
-  const clickHandler = () => {
-    // title = 'updated!';
-    setTtitle('Updated!');
-    console.log(title);
-  };
+  // const clickHandler = () => {
+  //   // title = 'updated!';
+  //   setTtitle('Updated!');
+  //   console.log(title);
+  // };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 };
