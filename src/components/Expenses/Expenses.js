17971d80f9d7b084a5eb16e9b6cf51582f483b9b
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-06-09 10:01:18 +0330

    add new

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,7 +5,14 @@ import './Expenses.css';
 const Expenses = (props) => {
   return (
     <Card className="expenses">
-      <ExpenseItem
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+      {/* <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
@@ -24,7 +31,7 @@ const Expenses = (props) => {
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
-      />
+      /> */}
     </Card>
   );
 };
